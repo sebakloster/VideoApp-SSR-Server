5555ed9512bf5b5dbbc57723a374635d43a6a626
Seba Kloster <sebikloster@yahoo.com.ar>
2021-04-26 10:40:30 -0300

    Agregando OAuth para hacer la autenticacion con google

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.use(cookieParser());
 //  Basic strategy
 require("./utils/auth/strategies/basic");
 
+//Oauth Strategy
+require("./utils/auth/strategies/oauth");
+
 // Agregamos las variables de timpo en segundos
 const THIRTY_DAYS_IN_SEC = 2592000;
 const TWO_HOURS_IN_SEC = 7200;
@@ -104,6 +107,32 @@ app.delete("/user-movies/:userMovieId", async function (req, res, next) {
     next(error);
   }
 });
+
+app.get(
+  "/auth/google-oauth",
+  passport.authenticate("google-oauth", {
+    scope: ["email", "profile", "openid"],
+  })
+);
+
+app.get(
+  "/auth/google-oauth/callback",
+  passport.authenticate("google-oauth", { session: false }),
+  function (req, res, next) {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie("token", token, {
+      httpOnly: !config.dev,
+      secure: !config.dev,
+    });
+
+    res.status(200).json(user);
+  }
+);
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
